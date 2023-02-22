088d78c71ecde8495ba67c51aee05a6aa1f3d087
chiOO7 <dr.chigoo@gmail.com>
2023-02-22 12:17:46 +0300

    fix some menu bugs, add hikari pool

--- src/main/java/ru/chislab/fireSystemTester/dao/ZoneDao.java
+++ src/main/java/ru/chislab/fireSystemTester/dao/ZoneDao.java
@@ -1,47 +1,51 @@
 package ru.chislab.fireSystemTester.dao;
 
-import jakarta.persistence.Query;
+//import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import ru.chislab.fireSystemTester.zones.Zone;
 
-import java.util.List;
+//import java.util.List;
 
 @RequiredArgsConstructor
 public class ZoneDao {
 
     private final SessionFactory sessionFactory;
 
-    public void saveZonesToStorage(List<Zone> zones) {
-        Session session = sessionFactory.openSession();
+//    public void saveZonesToStorage(List<Zone> zones) {
+//        Session session = sessionFactory.openSession();
+//
+//        session.beginTransaction();
+//        for (Zone zone : zones) {
+//            session.merge(zone);
+//        }
+//        session.getTransaction().commit();
+//    }
 
-        session.beginTransaction();
-        for (Zone zone : zones) {
-            session.merge(zone);
-        }
-        session.getTransaction().commit();
-    }
-
-    public List<Zone> getZonesFromStorage() {
-        Session session = sessionFactory.openSession();
-
-        session.beginTransaction();
-
-        Query query = session.createQuery("select z from Zone z", Zone.class);
-
-        session.getTransaction().commit();
-
-        return query.getResultList();
-    }
+//    public List<Zone> getZonesFromStorage() {
+//        Session session = sessionFactory.openSession();
+//
+//        session.beginTransaction();
+//
+//        Query query = session.createQuery("select z from Zone z", Zone.class);
+//
+//        session.getTransaction().commit();
+//
+//        return query.getResultList();
+//    }
 
     public void updateZone(Zone zone) {
-        Session session = sessionFactory.openSession();
+        if (zone == null) return;
 
-        session.beginTransaction();
+        if (sessionFactory != null) {
+            Session session = sessionFactory.openSession();
 
-        session.merge(zone);
+            session.beginTransaction();
 
-        session.getTransaction().commit();
+            session.merge(zone);
+
+            session.getTransaction().commit();
+        }
     }
 }
